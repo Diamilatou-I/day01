d79ab68372ef98a71bf2ae8bc8990918ba22e303
Diamilatou-I <diamilatou.diallo@epitech.digital>
2024-09-09 13:55:20 +0200

    Day1bis_Ex06.js

---
+++ Day1bis_Ex06.js
@@ -0,0 +1,9 @@
+if (bananasCount == 0) {
+displayThisText ("oh no, there is no banana")
+}
+else if (bananasCount > 0) {
+displayThisText ("yummy !")
+}
+else {
+displayThisText ("Do I owe you bananas?")
+}
